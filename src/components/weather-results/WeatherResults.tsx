520458510295f5c32de6a0cad3597113a406a08c
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2024-06-11 23:03:07 +0530

    customized graph styles

--- src/components/weather-results/WeatherResults.tsx
+++ src/components/weather-results/WeatherResults.tsx
@@ -1,10 +1,12 @@
 // import React from "react";
 import CurrentWeatherOverview from "./CurrentWeatherOverview";
+import HourlyWeatherGraph from "./HourlyWeatherGraph";
 
 function WeatherResults() {
   return (
     <div className="flex-1 p-5 rounded-md flex-shrink-0 shadow-md shadow-stone-100 min-h-28">
       <CurrentWeatherOverview />
+      <HourlyWeatherGraph />
     </div>
   );
 }
